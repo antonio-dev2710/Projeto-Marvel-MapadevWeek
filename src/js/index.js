be281e3821fa89289a6616980aa569798149f382
Antonio Alves <antoniocunha9610@gmail.com>
2022-05-20 00:21:40 -0300

    Atualização-Jquery

--- src/js/index.js
+++ src/js/index.js
@@ -5,49 +5,49 @@ let mouseHover = "thor";
 let selectedPlay1 = null;
 let selectedPlay2 = null;
 
-personagens.forEach((_personagem) => {
-  _personagem.addEventListener("mouseover", () => {
+personagens.forEach((_caracter) => {
+  _caracter.addEventListener("mouseover", () => {
       //Tratando efeitos jpogador 1
     if (!selectedPlay1) {
         //efeito de piscar em cima
       $("#" + mouseHover).toggleClass("selecionadoUm");
-      _personagem.classList.toggle("selecionadoUm");
+      _caracter.classList.toggle("selecionadoUm");
 
       //atualizando quem o mouse esta em cima
-      mouseHover = _personagem.getAttribute("id");
+      mouseHover = _caracter.getAttribute("id");
 
         //Trocando imagem
       $("#personagem-jogador-1").attr("src", `./src/imagens/${mouseHover}.png`);
 
       //Trocando nome
-      $("#nome-jogador-1").text(_personagem.getAttribute("data-name"));
+      $("#nome-jogador-1").text(_caracter.getAttribute("data-name"));
     }
     //Tratando efeitos jpogador 2
      else if(!selectedPlay2){
 
         //efeito de piscar em cima
       $("#" + mouseHover).toggleClass("selecionadoDois");
-      _personagem.classList.toggle("selecionadoDois");
+      _caracter.classList.toggle("selecionadoDois");
 
       //atualizando quem o mouse esta em cima
-      mouseHover = _personagem.getAttribute("id");
+      mouseHover = _caracter.getAttribute("id");
 
       //Trocando imagem
       $("#personagem-jogador-2").attr("src", `./src/imagens/${mouseHover}.png`);
 
       //Trocando nome
-      $("#nome-jogador-2").text(_personagem.getAttribute("data-name"));
+      $("#nome-jogador-2").text(_caracter.getAttribute("data-name"));
     }
   });
 
-  _personagem.addEventListener("click", () => {
+  _caracter.addEventListener("click", () => {
     if(!selectedPlay1) {
-        selectedPlay1 = _personagem.getAttribute("data-name");
+        selectedPlay1 = _caracter.getAttribute("data-name");
         $("#" + mouseHover).toggleClass("selecionadoDois");
         alert('Agora escolha o Play 2!')
     } else if(!selectedPlay2) {
         alert('GOGOGO')
-        selectedPlay2 = _personagem.getAttribute("data-name");
+        selectedPlay2 = _caracter.getAttribute("data-name");
     }
 
     //Trocando a label 1P para 2P , caso o player 1 ja foi selecionado e play 2 ainda não foi
@@ -55,7 +55,7 @@ personagens.forEach((_personagem) => {
         //Colocando texto 2P em todos os spans
         $(".tag").text("2P");
         //Deixando o texto 1P somente no persongem clicado na escolha do play 1
-        $(`#${_personagem.getAttribute("id")}`).children([0]).text("1P");
+        $(`#${_caracter.getAttribute("id")}`).children([0]).text("1P");
     }
   });
 });
